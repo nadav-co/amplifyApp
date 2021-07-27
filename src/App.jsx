4259d9f8f222b19fed06e954abbd970e9ac0c846
Nadav Cohen <nadav1999@gmail.com>
2021-07-27 21:48:51 +0300

    added auth

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+function App() {
+  return (
+    <div className="App">
+      <section className="auth">
+        <AmplifySignOut />
+      </section>
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
